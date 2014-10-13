4e62d16cc9fe07eec59e9f4516f52a1edf785f83
cheongil.hyun <cheongil.hyun@lge.com>
2014-10-13 10:59:00 +0900

    add configuration for yasnippet & auto complete mode

--- init.el
+++ init.el
@@ -141,6 +141,25 @@
 )
 
 ;; ----------------------------------------------------------------------------
+;; configuration for yasnippet
+;; should be loaded before auto complete so that they can work together
+;; ----------------------------------------------------------------------------
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; ----------------------------------------------------------------------------
+;; configuration for auto complete mod
+;; should be loaded after yasnippet so that they can work together
+;; ----------------------------------------------------------------------------
+(require 'auto-complete-config)
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+;; ----------------------------------------------------------------------------
 ;; configuration for enablling the recurive deleting directory
 ;; ----------------------------------------------------------------------------
 (setq dired-recursive-deletes 'top)	;; dired - recursive delete directory
