26da482b083bad4c91f1a2b6c58e56991626d336
cheongil.hyun <cheongil.hyun@lge.com>
2016-11-18 00:33:21 +0900

    Update the packages to use-package style

--- init.el
+++ init.el
@@ -1,22 +1,34 @@
 ;; ----------------------------------------------------------------------------
+;; adding the package sources
+;; ("marmalade" . "http://marmalade-repo.org/packages/") 
+;; ----------------------------------------------------------------------------
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(when (< 
+emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+  )
+(package-initialize)
+
+;; ----------------------------------------------------------------------------
 ;; adding better-defaults.el
 ;; ----------------------------------------------------------------------------
 (load-library "~/.emacs.d/better-defaults.el")
 (require 'better-defaults)
 
 ;; ----------------------------------------------------------------------------
-;; adding the package sources
+;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the reset of the packages
 ;; ----------------------------------------------------------------------------
-(if (< emacs-major-version 24)
-	(load-library "~/.emacs.d/packages/package.el")
-)
-(require 'package)
-;; (dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
-;; 				  ("melpa" . "http://melpa.milkbox.net/packages/")))
-;;   (add-to-list 'package-archives source t))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 ;; ----------------------------------------------------------------------------
 ;; display the column number on bottom bar
 ;; ----------------------------------------------------------------------------
@@ -59,17 +71,11 @@
     (load-theme 'wombat t)
 )
 
-;; ----------------------------------------------------------------------------
-;; adding xcscope.el
-;; ----------------------------------------------------------------------------
-(load-library "~/.emacs.d/packages/xcscope.el")
-(require 'xcscope)
-(setq cscope-do-not-update-database t)  ;; do not update the cscope files
 
 ;; ----------------------------------------------------------------------------
 ;; ediff-trees.elff
 ;; ----------------------------------------------------------------------------
-(load-file "~/.emacs.d/packages/ediff-trees.el")
+;; (load-file "~/.emacs.d/packages/ediff-trees.el")
 
 ;; ----------------------------------------------------------------------------
 ;; ediff set default to horizontally
@@ -108,145 +114,101 @@
 
 
 ;; ----------------------------------------------------------------------------
-;; org mode auto indent mode enable
+;; setting for company
 ;; ----------------------------------------------------------------------------
-(add-hook 'org-mode-hook
-          (lambda ()
-            (org-indent-mode t)
-			(auto-fill-mode 1)
-			)
-          t)
+(use-package company
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook 'company-mode)
+  (add-hook 'c-mode-hook 'company-mode)
+  (add-hook 'c++-mode-hook 'company-mode)
+
+  :config
+  (setq company-idle-delay 0.02)
+  (setq company-minimum-prefix-lenght 1)
+  (setq company-show-numbers t)
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence company-sort-by-backend-importance))
+  (setq company-tooltip-limit 20)
+  (setq company-dabbrev-downcase nil)
+  (setq company-dabbrev-ignore-case t)
+  (setq company-dabbrev-code-ignore-case t)
+  (setq company-dabbrev-code-everywhere t)
+)
+
+;;------------------------------------------------------------------------
+;; org mode Settings
+;;------------------------------------------------------------------------
+(use-package org
+  :ensure t
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (add-hook 'org-mode-hook
+			(lambda ()
+			  (org-indent-mode t)
+			  (auto-fill-mode 1)
+			  )
+			t)
+  (define-key global-map "\C-cl" 'org-store-link)
+  (define-key global-map "\C-ca" 'org-agenda)
+
+  :config
+  (setq org-log-done t)
+  ;; setting org files for org-agenda
+  (setq org-agenda-files (list "~/org/Task.org" "~/org/Schedule.org" "~/org/notes.org" "~/org/project.org"))
+  (setq org-directory "~/org/")
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  (define-key global-map "\C-cc" 'org-capture)
+  (setq org-capture-templates
+		'(("t" "Todo" entry (file+headline "~/org/Task.org" "Tasks")
+		   "* TODO %?\n  %i\n  %a")
+		  ("i" "Ideas" entry (file+datetree "~/org/notes.org")
+		   "* %?\nIdeas %U\n  %i\n  %a")))
+)
+
+;; ----------------------------------------------------------------------------
+;; adding xcscope.el
+;; ----------------------------------------------------------------------------
+(use-package xcscope
+  :load-path "~/.emacs.d/packages/"
+  :defer t
+  :init
+  (setq cscope-do-not-update-database t)  ;; do not update the cscope files
+)
 
 ;; ----------------------------------------------------------------------------
 ;; configuration for etags select
 ;; ----------------------------------------------------------------------------
-(load-file "~/.emacs.d/packages/etags-select.el")
-(require 'etags-select)
-(global-set-key "\M-?" 'etags-select-find-tag-at-point)
-(global-set-key "\M-." 'etags-select-find-tag)
+(use-package etags-select
+  :load-path "~/.emacs.d/packages/"
+  :defer t
+  :init
+  (global-set-key "\M-?" 'etags-select-find-tag-at-point)
+  (global-set-key "\M-." 'etags-select-find-tag)
+)
 
 ;; ----------------------------------------------------------------------------
 ;; C coding style setting now linux
-;; ----------------------------------------------------------------------------
-(setq c-default-style "linux")
 ;; (setq c-default-style "k&r")
 ;; (setq c-default-style "cc-mode")
 ;; (setq c-default-style "gnu")
 ;; (setq c-default-style "bsd")
 ;; (setq c-default-style "stroustrup")
 ;; (setq c-default-style "whitesmith")
+;; ----------------------------------------------------------------------------
+(setq c-default-style "linux")
 
 ;; ----------------------------------------------------------------------------
-;  korean font setting - cygwin emacs does not support
-;; ----------------------------------------------------------------------------
-(when window-system
-  (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("NanumGothicCoding" . "unicode-bmp"))
-  (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)  '("NanumGothicCoding" . "unicode-bmp"))
-  (set-fontset-font "fontset-default" 'kana '("Meiryo" . "unicode-bmp"))
-  (set-fontset-font "fontset-default" 'han '("Microsoft YaHei". "unicode-bmp"))
-)
-
 ;; (set-language-environment "Korean")
+;; ----------------------------------------------------------------------------
 (setq default-input-method "korean-hangul")
-
-;; ----------------------------------------------------------------------------
-;; configuration for yasnippet
-;; should be loaded before auto complete so that they can work together
-;; ----------------------------------------------------------------------------
-(require 'yasnippet)
-(yas-global-mode 1)
-
-;; ----------------------------------------------------------------------------
-;; configuration for auto complete mod
-;; should be loaded after yasnippet so that they can work together
-;; ----------------------------------------------------------------------------
-(require 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-;;; set the trigger key so that it can work together with yasnippet on tab key,
-;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;;; activate, otherwise, auto-complete will
-(global-auto-complete-mode t)
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
 
 ;; ----------------------------------------------------------------------------
 ;; configuration for enablling the recurive deleting directory
 ;; ----------------------------------------------------------------------------
 (setq dired-recursive-deletes 'top)	;; dired - recursive delete directory
-
-;; ----------------------------------------------------------------------------
-;; emacs server settings
-;; ----------------------------------------------------------------------------
-(when (equal window-system 'w32)
-  (require 'server)
-  ;; Suppress error directory ~/.emacs.d/server is unsafe on windows.
-  (if (equal window-system 'w32) (defun server-ensure-safe-dir (dir) "Noop" t))
-  (server-start)
-  (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
-)
-;; ----------------------------------------------------------------------------
-;; Cygwin Setting for cygwin-mount.el
-;; ----------------------------------------------------------------------------
-(when (eq window-system 'w32)
-  ;; (setenv "PATH" (concat "c:/cygwin/bin;" (getenv "PATH")))
-  ;; (setq exec-path (cons "c:/cygwin/bin/" exec-path))
-  (require 'cygwin-mount)
-  (cygwin-mount-activate)
-
-  ;; Replace the dos command window to bash of cygwin
-  (add-hook 'comint-output-filter-functions
-			'shell-strip-ctrl-m nil t)
-  (add-hook 'comint-output-filter-functions
-			'comint-watch-for-password-prompt nil t)
-  (setq explicit-shell-file-name "bash.exe")
-  ;; For subprocesses invoked via the shell
-  ;; (e.g., "shell -c command")
-  (setq shell-file-name explicit-shell-file-name)
-)
-;; ----------------------------------------------------------------------------
-;; configuration for coding system to unix default
-;; ----------------------------------------------------------------------------
-;; korean language setting
-(when (equal window-system 'w32)
-  (set-language-environment "Korean")
-  ;; (set-default-coding-systems 'utf-8-unix)
-  (prefer-coding-system 'euc-kr)
-  (set-default-coding-systems 'euc-kr)
-  (set-terminal-coding-system 'euc-kr)
-  (set-keyboard-coding-system 'euc-kr)
-  (set-selection-coding-system 'euc-kr)
-  (setq-default buffer-coding-system 'euc-kr)
-  (setq-default buffer-file-coding-system 'euc-kr)
-  (setq-default file-name-coding-system 'euc-kr)
-  (setq-default locale-coding-system 'euc-kr)
-  (add-hook 'sh-mode-hook'
-            (lambda ()
-              (set-default-coding-systems 'utf-8-unix)
-              ))
-  (setenv "CYGWIN" "nodosfilewarning")
-)
-
-;;------------------------------------------------------------------------
-;; org mode Settings
-;;------------------------------------------------------------------------
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-;; setting org files for org-agenda
-(setq org-agenda-files (list "~/org/Task.org" "~/org/Schedule.org" "~/org/notes.org" "~/org/project.org"))
-
-(setq org-directory "~/org/")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map "\C-cc" 'org-capture)
-
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/Task.org" "Tasks")
-		 "* TODO %?\n  %i\n  %a")
-        ("i" "Ideas" entry (file+datetree "~/org/notes.org")
-		 "* %?\nIdeas %U\n  %i\n  %a")))
 
 ;;------------------------------------------------------------------------
 ;; Dired+ Setting
@@ -265,50 +227,49 @@
 ;;------------------------------------------------------------------------
 ;; shell-pop
 ;;------------------------------------------------------------------------
-(load-file "~/.emacs.d/packages/shell-pop.el")
-(require 'shell-pop)
-(shell-pop-set-internal-mode "eshell")
-(shell-pop-set-internal-mode-shell "/bin/bash")
-(shell-pop-set-window-height 60) ; the number for the percentage of the selected window.
-(global-set-key [f10] 'shell-pop)
-
+(use-package shell-pop
+  :load-path  "~/.emacs.d/packages/"
+  :defer t
+  :config
+  (shell-pop-set-internal-mode "eshell")
+  (shell-pop-set-internal-mode-shell "/bin/bash")
+  (shell-pop-set-window-height 60) ; the number for the percentage of the selected window.
+  (global-set-key [f10] 'shell-pop)
+)
 ;;------------------------------------------------------------------------
 ;; shell-switcher
 ;;------------------------------------------------------------------------
-(require 'shell-switcher)
-(setq shell-switcher-mode t)
+(use-package shell-switcher
+  :ensure t
+  :defer t
+  :init
+  (setq shell-switcher-mode t)
+)
 
 ;;------------------------------------------------------------------------
 ;; mo-git-blame
 ;;------------------------------------------------------------------------
-(require 'mo-git-blame)
-(global-set-key [?\C-c ?g ?c] 'mo-git-blame-current)
-(global-set-key [?\C-c ?g ?f] 'mo-git-blame-file)
-
-;;------------------------------------------------------------------------
-;; elpy settings for python mode
-;; you should install first python, and pip
-;; sudo apt-get install python-pip
-;; sudo pip install elpy jedi rope
-;;------------------------------------------------------------------------
-(elpy-enable)
-(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
-
-;;------------------------------------------------------------------------
-;; iedit mode
-;;------------------------------------------------------------------------
-(define-key global-map (kbd "C-c ;") 'iedit-mode)
-
+(use-package mo-git-blame
+  :ensure t
+  :defer t
+  :init
+  (global-set-key [?\C-c ?g ?c] 'mo-git-blame-current)
+  (global-set-key [?\C-c ?g ?f] 'mo-git-blame-file)
+)
 ;;------------------------------------------------------------------------
 ;; magit speed up
 ;;------------------------------------------------------------------------
-(setq magit-highlight-indentation nil)
-(setq magit-highlight-trailing-whitespace nil)
-(setq magit-highlight-whitespace nil)
-(setq magit-diff-refine-hunk nil)
-(setq magit-backup-untracked nil)
-(setq magit-have-graph nil)
-
+(use-package magit
+  :ensure t
+  :defer t
+  :init
+  (setq magit-highlight-indentation nil)
+  (setq magit-highlight-trailing-whitespace nil)
+  (setq magit-highlight-whitespace nil)
+  (setq magit-diff-refine-hunk nil)
+  (setq magit-backup-untracked nil)
+  (setq magit-have-graph nil)
+)
 ;;------------------------------------------------------------------------
 ;; my key settings
 ;;------------------------------------------------------------------------
